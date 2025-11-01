9903ebb74b5235f3be33a53662ce5ed42806512c
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-02 00:17:19 +0100

    Update _evalcache_clear.fish

--- functions/_evalcache_clear.fish
+++ functions/_evalcache_clear.fish
@@ -1,12 +1,9 @@
 function _evalcache_clear
-    if test -z "$FISH_EVALCACHE_DIR"
-        set -gx FISH_EVALCACHE_DIR "$HOME/.fish-evalcache"
-    end
-
-    if test (count $argv) -eq 0
-        rm -i "$FISH_EVALCACHE_DIR"/init-*.fish
-    else
-        set -f cmd (basename $argv[1])
-        rm -i "$FISH_EVALCACHE_DIR/init-$cmd"-*.fish
-    end
+  test -z "$FISH_EVALCACHE_DIR"; and set -gx FISH_EVALCACHE_DIR "$HOME/.fish-evalcache"
+  if test (count $argv) -eq 0
+    rm -i "$FISH_EVALCACHE_DIR"/init-*.fish
+  else
+    set -l cmd (string split -m1 / $argv[1])[-1]
+    rm -i "$FISH_EVALCACHE_DIR/init-$cmd"-*.fish
+  end
 end
