8453a71edce9bb0b1725507a72926b74d6b498f7
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-02 09:49:18 +0100

    Update _evalcache.fish

--- functions/_evalcache.fish
+++ functions/_evalcache.fish
@@ -1,25 +1,31 @@
 function _evalcache
-  test (count $argv) -eq 0; and return
-  test -z "$FISH_EVALCACHE_DIR"; and set -gx FISH_EVALCACHE_DIR "$HOME/.fish-evalcache"
+  test (count $argv) -eq 0;and return
+  test -z "$FISH_EVALCACHE_DIR";and set -gx FISH_EVALCACHE_DIR "$HOME/.fish-evalcache"
   set -l cmd (string split -m1 / $argv[1])[-1]
   set -l cmdHash noHash
   if command -sq md5sum
-    set cmdHash (string join -- \n $argv | md5sum | string split -f1 ' ')
+    set cmdHash (string join -- \n $argv|md5sum|string split -f1 ' ')
   else if command -sq md5
-    set cmdHash (string join -- \n $argv | md5)
+    set cmdHash (string join -- \n $argv|md5)
   end
   set -l cacheFile "$FISH_EVALCACHE_DIR/init-$cmd-$cmdHash.fish"
   if test "$FISH_EVALCACHE_DISABLE" = true
-    eval ($argv | source)
+    $argv|source
   else if test -s "$cacheFile"
     source "$cacheFile"
   else if type -q $argv[1]
     echo "$argv[1] init not cached, caching: $argv" >&2
     mkdir -p "$FISH_EVALCACHE_DIR"
     rm -f $FISH_EVALCACHE_DIR/init-$cmd-*.fish 2>/dev/null
-    $argv >$cacheFile
-    source "$cacheFile"
+    if $argv >$cacheFile 2>&1
+      source "$cacheFile"
+    else
+      echo "evalcache ERROR: $argv[1] failed" >&2
+      rm -f "$cacheFile"
+      return 1
+    end
   else
-    echo "evalcache ERROR: $cmd not in PATH" >&2
+    echo "evalcache ERROR: $argv[1] not in PATH" >&2
+    return 1
   end
 end
